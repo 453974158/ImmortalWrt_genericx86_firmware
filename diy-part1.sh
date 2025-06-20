127b2d68a567770d29c97ec9a5d4acd9b23adb9e
Mr.Blcakcat <q453974158@gmail.com>
2025-06-21 00:22:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,6 +18,8 @@ sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git smartdns https://github.com/pymumu/openwrt-smartdns' >>feeds.conf.default
+echo 'src-git luci-app-smartdns https://github.com/pymumu/luci-app-smartdns' >>feeds.conf.default
 
 #luci-app-temp-status 温度显示
 git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
