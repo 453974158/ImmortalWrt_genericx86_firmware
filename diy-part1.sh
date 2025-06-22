eb8174c22c69fcde4fd3d8150363699fb6820da7
Mr.Blcakcat <q453974158@gmail.com>
2025-06-22 18:22:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,6 +21,3 @@ sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 #luci-app-temp-status 温度显示
 git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
-
-#smartdns
-git clone https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
